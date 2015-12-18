e36fcababfbaf22e22d6d443d76113fc0881cc66
Afjal <afjal.ali@softageindia.com>
2015-12-18 18:10:52 +0530

    commit for spring hibernate integration
    Change-Id: I338c72913a2f560c6a07f4ed59727c73e33d6fbd
    Signed-off-by: Afjal <afjal.ali@softageindia.com>

--- src/main/java/com/softage/paytm/models/Employee.java
+++ src/main/java/com/softage/paytm/models/Employee.java
@@ -6,86 +6,86 @@ import javax.persistence.Id;
 
 @Entity(name="EmpLoginTable")
 public class Employee {
-	
+
 	@Id
-	@Column(name="EmpPhone" ,unique=true)
-	private String EmpPhone;
-	
-	private String Emp_code ;
-	private String Emp_Name;
-	private String EmpPassword;
-	private String Role_Code;
-	private String Emp_Status;
-	private String ImportBy;
-	private String ImportDate;
-	private long Cir_code;
-	private String Roles;
-	private String ssoScope;
-	
-	public String getSsoScope() { return ssoScope; }
-	public void setSsoScope(String ssoScope){ ssoScope = this.ssoScope; }
-	public String getEmp_code() {
+	@Column(unique=true)
+	private int EmpPhone;
+
+private String Emp_code ;
+private String Emp_Name;
+private String EmpPassword;
+private String Role_Code;
+private String Emp_Status;
+private String ImportBy;
+private String ImportDate;
+private long Cir_code;
+private String Roles;
+private String ssoScope;
+
+public String getSsoScope() { return ssoScope; }
+public void setSsoScope(String ssoScope){ ssoScope = this.ssoScope; }
+public String getEmp_code() {
 		return Emp_code;
-	}
-	public void setEmp_code(String emp_code) {
+		}
+public void setEmp_code(String emp_code) {
 		Emp_code = emp_code;
-	}
-	public String getEmp_Name() {
+		}
+public String getEmp_Name() {
 		return Emp_Name;
-	}
-	public void setEmp_Name(String emp_Name) {
+		}
+public void setEmp_Name(String emp_Name) {
 		Emp_Name = emp_Name;
-	}
-	public String getEmpPhone() {
+		}
+public int getEmpPhone() {
 		return EmpPhone;
-	}
-	public void setEmpPhone(String empPhone) {
+		}
+public void setEmpPhone(int empPhone) {
 		EmpPhone = empPhone;
-	}
-	public String getEmpPassword() {
+		}
+public String getEmpPassword() {
 		return EmpPassword;
-	}
-	public void setEmpPassword(String empPassword) {
+		}
+public void setEmpPassword(String empPassword) {
 		EmpPassword = empPassword;
-	}
-	public String getRole_Code() {
+		}
+public String getRole_Code() {
 		return Role_Code;
-	}
-	public void setRole_Code(String role_Code) {
+		}
+public void setRole_Code(String role_Code) {
 		Role_Code = role_Code;
-	}
-	public String getEmp_Status() {
+		}
+public String getEmp_Status() {
 		return Emp_Status;
-	}
-	public void setEmp_Status(String emp_Status) {
+		}
+public void setEmp_Status(String emp_Status) {
 		Emp_Status = emp_Status;
-	}
-	public String getImportBy() {
+		}
+public String getImportBy() {
 		return ImportBy;
-	}
-	public void setImportBy(String importBy) {
+		}
+public void setImportBy(String importBy) {
 		ImportBy = importBy;
-	}
-	public String getImportDate() {
+		}
+public String getImportDate() {
 		return ImportDate;
-	}
-	public void setImportDate(String importDate) {
+		}
+public void setImportDate(String importDate) {
 		ImportDate = importDate;
-	}
-	public long getCir_code() {
+		}
+public long getCir_code() {
 		return Cir_code;
-	}
-	public void setCir_code(long cir_code) {
+		}
+public void setCir_code(long cir_code) {
 		Cir_code = cir_code;
-	}
-	public String getRoles() {
+		}
+public String getRoles() {
 		return Roles;
-	}
-	public void setRoles(String roles) {
+		}
+public void setRoles(String roles) {
 		Roles = roles;
-	}
-	
-	
-	
-	
-}
+		}
+
+
+
+
+		}
